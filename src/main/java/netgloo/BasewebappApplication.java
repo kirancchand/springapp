940f818f53133b8abfbfb00ef7c79ff5ba27fe47
kirancchand <kirancchand@gmail.com>
2019-09-18 17:15:08 +0530

    first

---
+++ src/main/java/netgloo/BasewebappApplication.java
@@ -0,0 +1,13 @@
+package netgloo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasewebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasewebappApplication.class, args);
+	}
+
+}
